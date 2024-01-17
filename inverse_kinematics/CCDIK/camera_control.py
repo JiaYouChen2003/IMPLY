d08e73d63a991868361f0b546054cb36943bf458
Jia-You Chen <113786162+JiaYouChen2003@users.noreply.github.com>
2024-01-18 00:31:04 +0100

    Revert ":sparkles: Add arm_demo.py"
    This reverts commit d3d6d73b885beb539778a8ceee6caa6f5f6523a3.

--- inverse_kinematics/CCDIK/camera_control.py
+++ inverse_kinematics/CCDIK/camera_control.py
@@ -35,11 +35,11 @@ class CameraControl:
 
         self.speed = speed
 
-        self.zoom = 800
+        self.zoom = 10
 
 
-        self.heading = 180
-        self.pitch = -60
+        self.heading = 0
+        self.pitch = -45
 
         self.node.set_pos( 0, -self.zoom, 0 )
         self.pitch_node.set_hpr( 0, self.pitch, 0 )
